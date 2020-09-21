caaffcb9a9dfe0cbfcc80786ab16e40bbd4cf7d7
Corey Martin <coreyjmartin2@gmail.com>
2020-09-21 19:22:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,9 @@ class Transfer
   attr_reader :account, new_account
   
   def initialize(new_account, account, balance)
-    
+    @new_account = new_account
+    @account = account
+    @balance = balance
     
   end
 end
