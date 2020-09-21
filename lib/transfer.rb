c8104d0287c4844b1a5bd3d1ba2bb0ad6db74ddd
Corey Martin <coreyjmartin2@gmail.com>
2020-09-21 19:39:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,8 @@ class Transfer
       receiver.balance += amount
       @status = "pending"
     else
-      
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
